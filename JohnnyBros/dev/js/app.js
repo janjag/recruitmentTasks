643c4ef1be02477864ec5afb45c868688fad02a7
Jan Jagieła <janjag@humanistcanco.de>
2018-08-04 22:36:48 +0200

    - added media querys for smaller screans up to tablets (768px wide) - added basic js

--- JohnnyBros/dev/js/app.js
+++ JohnnyBros/dev/js/app.js
@@ -1,5 +1,42 @@
 'use strict';
 
 (function() {
-    
+    document.querySelector('.menu-toggle').addEventListener('click', toggleMenu, false);
+
+    function toggleMenu() {
+        event.target.classList.toggle('is-open');
+    }
+
+    window.addEventListener('scroll', scroller, false);
+    window.addEventListener('touchmove', scroller, false);
+
+    function scroller () {
+        var curent_pos = window.pageYOffset;
+        var height = document.querySelector('.header').offsetHeight;
+        var sections = document.querySelectorAll('.section');
+
+        if (curent_pos >= height) {
+            document.querySelector('.navbar').classList.add('fixed');
+        } else {
+            document.querySelector('.navbar').classList.remove('fixed');
+        }
+
+        sections.forEach(function(el) {
+            var top = el.offsetTop - (.25 * window.innerHeight);
+            var bottom = top + el.offsetHeight;
+
+            if (curent_pos >= top && curent_pos <= bottom) {
+                document.querySelectorAll('.nav-link').forEach(function(link) {
+                    link.classList.remove('active');
+                });
+
+                var hash = '#' + el.getAttribute('id');
+                if(window.location.hash !== hash) {
+                    history.pushState(null, null, hash);
+                }
+
+                document.querySelector('a[href="' + hash + '"]').classList.add('active');
+            }
+        });
+    }
 })();
